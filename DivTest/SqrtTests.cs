f48e7a2788ac6beeeda34b7ab0c170aab8f0c9df
Podv94 <69922023+Podv94@users.noreply.github.com>
2020-08-20 01:26:18 +0600

    Add test and WEB
    Add test and WEB

---
+++ DivTest/SqrtTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ObjectOrientedCalculator.Operations;
+
+namespace ObjectOrientedCalculator.Tests.Operations
+{
+    [TestFixture]
+    public class SqrtTests
+    {
+        [Test]
+        public void CalculateTest()
+        {
+            Sqrt calculator = new Sqrt();
+            Assert.AreEqual(5, calculator.Calculate(25));
+            Assert.AreEqual(3, calculator.Calculate(9));
+        }
+    }
+}
